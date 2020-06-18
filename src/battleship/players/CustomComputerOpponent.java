f37453a746ad26f007d3b18cac1f49cec4436b46
Birte Glimm <birte.glimm@uni-ulm.de>
2020-06-18 14:39:28 +0200

    changed name

--- src/battleship/players/CustomComputerOpponent.java
+++ src/battleship/players/CustomComputerOpponent.java
@@ -22,7 +22,7 @@ public class CustomComputerOpponent extends ComputerOpponent {
      */
     @Override
     public String toString() {
-        return name + " (Custom AI)";
+        return name + " (My Custom Clever AI)";
     }
 
     // ------------------------------------------------------------------------------------------------
